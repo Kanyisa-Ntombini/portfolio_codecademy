ea04a8a64c7f22260fd731d40afaaaf2c5e1f795
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2022-04-21 20:01:45 +0200

    added interactivity to view projects

---
+++ resources/js/script.js
@@ -0,0 +1,49 @@
+const flexboxBusiness = document.getElementById('flexboxBusiness');
+flexboxBusiness.click = false;
+const hideFlexboxBusiness = document.getElementById('hideFlexboxBusiness');
+const flexboxCloseBtn = document.getElementById('flexboxBtn');
+
+const mysteryOrganism = document.getElementById('mysteryOrganism');
+mysteryOrganism.click = false;
+const hideMysteryOrganism = document.getElementById('hideMysteryOrganism');
+const mysteryOrganismCloseBtn = document.getElementById('mysteryOrganismBtn');
+
+const chatbot = document.getElementById('chatbot');
+chatbot.click = false;
+const hideChatbot = document.getElementById('hideChatbot');
+const chatbotCloseBtn = document.getElementById('chatbotBtn');
+
+const printProject = event => {
+  event.target.click = true;
+  if (flexboxBusiness.click === true) {
+    hideFlexboxBusiness.style.display = 'block';
+  } else if (mysteryOrganism.click === true) {
+    hideMysteryOrganism.style.display = 'block';
+  }  else if (chatbot.click === true) {
+    hideChatbot.style.display = 'block';
+  }
+}
+
+const closeProject = event => {
+  if (flexboxBusiness.click === true) {
+    hideFlexboxBusiness.style.display = 'none';
+    flexboxBusiness.click = false;
+  } else if (mysteryOrganism.click === true) {
+    hideMysteryOrganism.style.display = 'none';
+    mysteryOrganism.click = false;
+  } else if (chatbot.click === true) {
+    hideChatbot.style.display = 'none';
+    chatbot.click = false;
+  }
+}
+
+flexboxBusiness.addEventListener('click', printProject);
+flexboxCloseBtn.addEventListener('click', closeProject);
+
+mysteryOrganism.addEventListener('click', printProject);
+mysteryOrganismCloseBtn.addEventListener('click', closeProject);
+
+chatbot.addEventListener('click', printProject);
+chatbotCloseBtn.addEventListener('click', closeProject);
+
+console.log(hideChatbot);
